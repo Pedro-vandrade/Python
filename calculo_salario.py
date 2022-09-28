3f1c9113d15248b07cb2c4c8d6bf38ea799a4b09
Pedro Vargas <96157565+Pedro-vandrade@users.noreply.github.com>
2022-09-28 00:27:54 -0300

    Add files via upload

--- calculo_salario.py
+++ calculo_salario.py
@@ -1,13 +1,19 @@
 lista_salarios = []
+lista_15 = []
+lista_10 = []
 while True:
     salario = float(input("Digite o salário do funcionário ou 0 para sair: "))
-    lista_salarios.append(salario)
+    if salario > 0:
+        lista_salarios.append(salario)
     if salario <= 1250.00:
         sal15 = salario + (salario * 15 / 100)
+        lista_15.append(sal15)
         print(f'Seu novo salário será de {sal15}, com aumento de 15%.')
     if salario > 1250.00:
         sal10 = salario + (salario * 10 / 100)
+        lista_10.append(sal10)
         print(f"seu novo salário de de {sal10}, com aumento de 10%")
     if salario == 0:
-        print(lista_salarios)
+        print(f'Salários com aumento de 15%: {lista_15}')
+        print(f'Salários com aumento de 10%: {lista_10}')
         print("Fim do Programa")
